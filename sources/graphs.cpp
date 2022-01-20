413e40d858998f92124dd37a72c67bb8392c716a
EinHerrSchmidt <matte24694@gmail.com>
2022-01-20 15:39:48 +0100

    Fix: do not repeat classes' definitions, just provide implementations.

--- sources/graphs.cpp
+++ sources/graphs.cpp
@@ -3,56 +3,40 @@
 #include <fstream>
 #include <set>
 #include <string>
+#include "graphs.h"
 
 using namespace std;
 
-class Edge
+Edge::Edge(int n1, int n2)
 {
-private:
-    int v1;
-    int v2;
+    v1 = n1;
+    v2 = n2;
+}
 
-public:
-    Edge(int n1, int n2)
-    {
-        v1 = n1;
-        v2 = n2;
-    }
-
-    int getV1()
-    {
-        return v1;
-    }
-
-    int getV2()
-    {
-        return v2;
-    }
-
-    bool operator<(const Edge &e) const
-    {
-        return v1 < e.v1 || (v1 == e.v1 && v2 < e.v2);
-    }
-};
-
-class Graph
+int Edge::getV1()
 {
-protected:
-    int totalVertices;
-    vector<set<int>> neighbors;
-    friend std::ostream &operator<<(std::ostream &, const Graph &);
+    return v1;
+}
 
-public:
-    int size()
-    {
-        return totalVertices;
-    }
+int Edge::getV2()
+{
+    return v2;
+}
 
-    vector<set<int>> getAdjacencyLists()
-    {
-        return neighbors;
-    }
-};
+bool Edge::operator<(const Edge &e) const
+{
+    return v1 < e.v1 || (v1 == e.v1 && v2 < e.v2);
+}
+
+int Graph::size()
+{
+    return totalVertices;
+}
+
+vector<set<int>> Graph::getAdjacencyLists()
+{
+    return neighbors;
+}
 
 std::ostream &operator<<(std::ostream &strm, const Graph &graph)
 {
@@ -75,89 +59,77 @@ std::ostream &operator<<(std::ostream &strm, const Graph &graph)
     return strm << graphAsString;
 }
 
-class UndirectedGraph : public Graph
+UndirectedGraph::UndirectedGraph(int v, set<Edge> edges)
 {
-public:
-    UndirectedGraph(int v, set<Edge> edges)
+    totalVertices = v;
+    for (int i = 0; i < totalVertices; i++)
     {
-        totalVertices = v;
-        for (int i = 0; i < totalVertices; i++)
-        {
-            set<int> s;
-            neighbors.push_back(s);
-        }
-
-        for (Edge e : edges)
-        {
-            neighbors[e.getV1()].insert(e.getV2());
-            neighbors[e.getV2()].insert(e.getV1());
-        }
+        set<int> s;
+        neighbors.push_back(s);
     }
-};
 
-class DirectedGraph : public Graph
-{
-public:
-    DirectedGraph(int v, set<Edge> edges)
+    for (Edge e : edges)
     {
-        totalVertices = v;
-        for (int i = 0; i < totalVertices; i++)
-        {
-            set<int> s;
-            neighbors.push_back(s);
-        }
-
-        for (Edge e : edges)
-        {
-            neighbors[e.getV1()].insert(e.getV2());
-        }
+        neighbors[e.getV1()].insert(e.getV2());
+        neighbors[e.getV2()].insert(e.getV1());
     }
-};
+}
 
-class GraphFactory
+DirectedGraph::DirectedGraph(int v, set<Edge> edges)
 {
-private:
-    pair<int, set<Edge>> *verticesAndEdgeSetFromFile(string inputFile)
+    totalVertices = v;
+    for (int i = 0; i < totalVertices; i++)
     {
-        int totalVertices = 0;
-        set<Edge> edges;
-        string line;
-        ifstream file(inputFile);
-        if (file.is_open())
+        set<int> s;
+        neighbors.push_back(s);
+    }
+
+    for (Edge e : edges)
+    {
+        neighbors[e.getV1()].insert(e.getV2());
+    }
+}
+
+pair<int, set<Edge>> *GraphFactory::verticesAndEdgeSetFromFile(string inputFile)
+{
+    int totalVertices = 0;
+    set<Edge> edges;
+    string line;
+    string filePath = "..\\data\\" + inputFile;
+    ifstream file(filePath);
+    if (file.is_open())
+    {
+        // first line contains number of vertices in the graph
+        getline(file, line);
+        totalVertices = stoi(line);
+        while (getline(file, line))
         {
-            // first line contains number of vertices in the graph
-            getline(file, line);
-            totalVertices = stoi(line);
-            while (getline(file, line))
+            size_t pos = 0;
+            string edgeVertices[2];
+            char delimiter = '\t';
+            int tokenPosition = 0;
+            // std::string::npos -> Value returned by various member functions when they fail.
+            pos = line.find(delimiter);
+            while ((pos = line.find(delimiter)) != std::string::npos)
             {
-                size_t pos = 0;
-                string edgeVertices[2];
-                char delimiter = '\t';
-                int tokenPosition = 0;
-                // std::string::npos -> Value returned by various member functions when they fail.
-                pos = line.find(delimiter);
-                while ((pos = line.find(delimiter)) != std::string::npos)
-                {
-                    edgeVertices[tokenPosition++] = line.substr(0, pos);
-                    line.erase(0, pos + 1); // + 1 -> delimiter's length
-                }
-                edgeVertices[tokenPosition] = line.substr(pos + 1, std::string::npos);
-                edges.insert(Edge(stoi(edgeVertices[0]), stoi(edgeVertices[1])));
+                edgeVertices[tokenPosition++] = line.substr(0, pos);
+                line.erase(0, pos + 1); // + 1 -> delimiter's length
             }
+            edgeVertices[tokenPosition] = line.substr(pos + 1, std::string::npos);
+            edges.insert(Edge(stoi(edgeVertices[0]), stoi(edgeVertices[1])));
         }
-        return new pair<int, set<Edge>>(totalVertices, edges);
     }
+    return new pair<int, set<Edge>>(totalVertices, edges);
+}
 
-public:
-    UndirectedGraph *createUndirectedGraphFromFile(string inputFile)
-    {
-        pair<int, set<Edge>> verticesAndEdges = *this->verticesAndEdgeSetFromFile(inputFile);
-        return new UndirectedGraph(verticesAndEdges.first, verticesAndEdges.second);
-    }
+UndirectedGraph *GraphFactory::createUndirectedGraphFromFile(string inputFile)
+{
+    pair<int, set<Edge>> verticesAndEdges = *this->verticesAndEdgeSetFromFile(inputFile);
+    return new UndirectedGraph(verticesAndEdges.first, verticesAndEdges.second);
+}
 
-    DirectedGraph *createDirectedGraphFromFile(string inputFile)
-    {
-        pair<int, set<Edge>> verticesAndEdges = *this->verticesAndEdgeSetFromFile(inputFile);
-        return new DirectedGraph(verticesAndEdges.first, verticesAndEdges.second);
-    }
-};
+DirectedGraph *GraphFactory::createDirectedGraphFromFile(string inputFile)
+{
+    pair<int, set<Edge>> verticesAndEdges = *this->verticesAndEdgeSetFromFile(inputFile);
+    return new DirectedGraph(verticesAndEdges.first, verticesAndEdges.second);
+}
