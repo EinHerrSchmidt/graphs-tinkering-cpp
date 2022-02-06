da39e7d54a9da07f72df6f3ad6c5ddf0a0b4048b
EinHerrSchmidt <matte24694@gmail.com>
2022-02-06 15:05:01 +0100

    Main cleaning.

--- sources/main.cpp
+++ sources/main.cpp
@@ -21,44 +21,24 @@ int main()
     Vertex<std::string> c1(2, "C", "c");
     Vertex<std::string> d1(3, "D", "d");
     Vertex<std::string> e1(4, "E", "e");
-    std::set<Vertex<std::string>> dgVertices = {a1, b1, c1, d1, e1};
+    std::set<Vertex<std::string>> vertices = {a1, b1, c1, d1, e1};
     Edge<std::string> e11(a1, b1);
     Edge<std::string> e21(b1, c1);
     Edge<std::string> e31(d1, a1);
     Edge<std::string> e41(d1, c1);
-    std::set<Edge<std::string>> dgEdges = {e11, e21, e31, e41};
+    std::set<Edge<std::string>> edges = {e11, e21, e31, e41};
 
-    DirectedGraph dgManual(dgVertices, dgEdges);
+    DirectedGraph dgManual(vertices, edges);
 
     std::cout << "(Manual) Directed Graph:"
               << "\n\n"
               << dgManual << "\n";
 
-    Vertex<std::string> a2(0, "A", "a");
-    Vertex<std::string> b2(1, "B", "b");
-    Vertex<std::string> c2(2, "C", "c");
-    Vertex<std::string> d2(3, "D", "d");
-    Vertex<std::string> e2(4, "E", "e");
-    std::set<Vertex<std::string>> udgVertices = {a2, b2, c2, d2, e2};
-    Edge<std::string> e12(a2, b2);
-    Edge<std::string> e22(b2, c2);
-    Edge<std::string> e32(d2, a2);
-    Edge<std::string> e42(d2, c2);
-    std::set<Edge<std::string>> udgEdges = {e12, e22, e32, e42};
-
-    UndirectedGraph udgManual(udgVertices, udgEdges);
+    UndirectedGraph udgManual(vertices, edges);
 
     std::cout << "(Manual) Undirected Graph:"
               << "\n\n"
               << udgManual;
-
-    Vertex<std::string> tf1(0, "A", "a");
-    Vertex<std::string> tf2(3, "D", "d");
-    Edge<std::string> etf(tf2, tf1);
-    Edge<std::string> e = *udgManual.getEdges().find(etf);
-
-    std::cout << e.getV1().getId() << " " << e.getV1().getLabel() << " " << e.getV1().getValue() << "\n";
-    std::cout << e.getV2().getId() << " " << e.getV2().getLabel() << " " << e.getV2().getValue() << "\n";
 
     GraphFactory<std::string> gf;
 
